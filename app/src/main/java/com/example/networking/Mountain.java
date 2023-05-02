1171bd68fbd784217c3d37d3891413fdaaf4c6ba
ChristianFast <112157470+ChristianFast@users.noreply.github.com>
2023-05-02 12:09:33 +0200

    1. Added ArrayList<Mountain> mountains to main activity. 2. Created the class for mountains and added "name" member with getters/setters.

---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,13 @@
+package com.example.networking;
+
+public class Mountain {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    private String name;
+}
